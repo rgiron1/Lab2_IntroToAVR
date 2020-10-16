e701ead0ceb2ab9d6d1eab7868025a91aed72aaf
Ryan Giron <rgiro004@wch136-24.cs.ucr.edu>
2020-10-15 18:31:05 -0700

    Completed part 3

---
+++ turnin/rgiro004_lab2_part3.c
@@ -0,0 +1,39 @@
+/*	Author: rgiro004
+ *  Partner(s) Name: Jason Shweiri
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xCC; PORTC = 0xFF;
+	unsigned char cntavail = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	cntavail = 0x04;
+	if(cntavail & 0x01){
+		cntavail--;
+	} else if(cntavail & 0x02){
+		cntavail--;
+	} else if(cntavail & 0x04){
+		cntavail--;
+	} else if(cntavail & 0x08){
+		cntavail--;
+	}
+	PORTC = cntavail;
+	if(cntavail == 0x00){
+		PORTC = 0x80;
+	}
+
+    }
+    return 0;
+}
